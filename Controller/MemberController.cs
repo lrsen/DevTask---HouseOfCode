23150f159db37bb225d9104259813e252f125442
Lrsen <mortenlrsen@gmail.com>
2019-07-13 17:32:39 +0200

    Add project files.

---
+++ Controller/MemberController.cs
@@ -0,0 +1,65 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using Umbraco.Web.Mvc;
+using DevTask___HouseOfCode.Models;
+
+namespace DevTask___HouseOfCode.Controllers
+{
+    public class MemberController : SurfaceController
+    {
+        public ActionResult RenderLogin()
+        {
+            return PartialView("_Login", new LoginModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitLogin(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.Username, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    UrlHelper myHelper = new UrlHelper(HttpContext.Request.RequestContext);
+                    if (myHelper.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return Redirect("/");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The username or password provided is incorrect.");
+                }
+            }
+            return CurrentUmbracoPage();
+        }
+
+        public ActionResult RenderLogout()
+        {
+            return PartialView("_Logout", null);
+        }
+
+        public ActionResult SubmitLogout()
+        {
+            TempData.Clear();
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return Redirect("/");
+        }
+
+        public ActionResult RenderSignUp()
+        {
+            return PartialView("_SignUpPartial", null);
+        }
+
+        public ActionResult RenderProduct()
+        {
+            return PartialView("_ProductsShowcase", null);
+        }
+    }
+}
